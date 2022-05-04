e6430afedc46f2ba12bb7e2c629eef418946416b
chenguo-design <20185227077@stu.suda.edu.cn>
2022-05-04 23:32:11 +0800

    create project

--- src/main/java/com/learn/test/DailyTest.java
+++ src/main/java/com/learn/test/DailyTest.java
@@ -1,4 +1,17 @@
 package com.learn.test;
 
-public class Test {
+import org.junit.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class DailyTest {
+
+    @Test
+    public void test_reflect() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Class<Integer> integerClass = int.class;
+        Constructor<Integer> constructor = integerClass.getConstructor();
+        Integer integer = constructor.newInstance(2);
+        System.out.println(integer);
+    }
 }
